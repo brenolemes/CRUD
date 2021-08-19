2b4886ebdc0e6ec50f35387445c50e1654961287
Ricardo <eu@ricardok.com.br>
2021-08-19 16:48:34 -0300

    Adicionando o arquivo CSS

--- menu.php
+++ menu.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilo.css">
     <title>Menu</title>
 </head>
 <body>
-    <a href="consulta_bd.php">Consultar os dados dos clientes cadastrados</a> 
-    <br>
-    <a href="cadastro.php">Cadastrar um novo cliente</a> 
-    <br>
-    <a href="atualiza.php">Atualizar um cliente</a> 
-    <br>
-    <a href="deleta.php">Deletar um cliente</a> 
-    <br>
+    <div class="menu">
+        <a href="consulta_bd.php">Consultar os dados dos clientes cadastrados</a> 
+        <br>
+        <a href="cadastro.php">Cadastrar um novo cliente</a> 
+        <br>
+        <a href="atualiza.php">Atualizar um cliente</a> 
+        <br>
+        <a href="deleta.php">Deletar um cliente</a> 
+        <br>
+    </div>
 </body>
 </html>
